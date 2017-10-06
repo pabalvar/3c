687b038d7b37664ea0352ede7df9a6c4148cc381
pabalvar <pabalvar@b1d87855-21b7-1442-897f-3fc862daeb18>
2017-10-06 13:57:55 +0000

    saca ruta se incluye en express

--- app/lib/globals/routes/globals.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var globals = require('../../../../app/lib/globals/controllers/globals.js');
-var _ = require("lodash");
-/** Middleare para normalizar parámetros de query de la URL **/
-module.exports = function(app) {
-    // Convierte parámetros de URL en forma segura
-    app.use(globals.urlParams);
-}
