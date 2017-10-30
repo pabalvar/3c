68cd6a2de956d52313e75226db866ad0fb3b3b59
pabalvar <pabalvar@b1d87855-21b7-1442-897f-3fc862daeb18>
2017-10-30 21:15:09 +0000

    incorpora variante

--- app/liberp/pagos.js
+++ app/liberp/pagos.js
@@ -9,7 +9,21 @@ function getPagos(req, res, next) {
     // en este contecto req.params.id es idmaedpce
     if (req.params.id) req.query.idmaedpce = req.params.id;
 
-    req.query.id = req.params.id;
+    // tidp por defecto es la lista abajo
+    req.query.tidp = req.query.tidp||true;
+
+    // variante: simple|complejo
+    switch (req.query.variante) {
+        case 'simple':
+            req.query.variante = "AND (DPCE.ESPGDP<> 'N' AND DPCE.ESASDP = 'P')" + '/noquote';
+            break;
+        case 'general':
+            req.query.variante = "AND ((DPCE.ESPGDP<> 'N' AND DPCE.ESASDP = 'P') OR ( DPCE.ESPGDP = 'P' OR DPCE.ESPGDP = 'R' ))" + '/noquote';
+            break;
+        default:
+            req.query.variante = ''; // Necesario para que no inyecten SQL
+    }
+
     req.consultas.pagos = SQLcast(getPagosQuerySQL(), req.query, req.pagination);
     next();
 }
@@ -48,9 +62,10 @@ WHERE 1=1
 AND DPCE.IDMAEDPCE IN ('192')--<< idmaedpce
 AND DPCE.EMPRESA IN ('01')--<< empresa
 AND DPCE.ENDP IN  ('001')--<< koen
-AND DPCE.TIDP IN  ('FCV')--<< tidp
+AND DPCE.TIDP IN  ('ATB','CHV','CPV','CRV','DEP','EFV','LTV','PAV','RBV','RGV','RIV','TJV','VUE') --<< tidp
 AND DPCE.ESASDP IN ('P')--<< esasdp
-AND DPCE.ESPGDP = 'N'--<< espgdp
+AND DPCE.ESPGDP IN ('N')--<< espgdp
+AND ( DPCE.ESPGDP = 'P' OR DPCE.ESPGDP = 'R' ) --<< variante
 -->> order
 ORDER BY 
 DPCE.FEEMDP
