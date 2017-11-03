59d193fecdbf6300cb18643cd5c204c0ba1e8cc2
pabalvar <pabalvar@b1d87855-21b7-1442-897f-3fc862daeb18>
2017-11-03 12:07:15 +0000

    gruntfile WARNING: reduce a lo que se necesita

--- gruntfile.js
+++ gruntfile.js
@@ -1,171 +1,44 @@
-'use strict';
+module.exports = function (grunt) {
 
-module.exports = function(grunt) {
-    // Unified Watch Object
-    var watchFiles = {
-        serverViews: ['app/views/**/*.*'],
-        serverJS: ['gruntfile.js', 'server.js', 'config/**/*.js', 'app/**/*.js'],
-        clientViews: ['public/modules/**/views/**/*.html'],
-        clientJS: ['public/js/*.js', 'public/modules/**/*.js'],
-        clientCSS: ['public/modules/**/*.css']
-    };
-
-    // Project Configuration
+    // Project configuration.
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-        watch: {
-            serverViews: {
-                files: watchFiles.serverViews,
-                options: {
-                    livereload: true
-                }
-            },
-            serverJS: {
-                files: watchFiles.serverJS,
-                tasks: ['jshint'],
-                options: {
-                    livereload: true
-                }
-            },
-            clientViews: {
-                files: watchFiles.clientViews,
-                options: {
-                    livereload: true,
-                }
-            },
-            clientJS: {
-                files: watchFiles.clientJS,
-                tasks: ['jshint'],
-                options: {
-                    livereload: true
-                }
-            },
-            clientCSS: {
-                files: watchFiles.clientCSS,
-                tasks: ['csslint'],
-                options: {
-                    livereload: true
-                }
-            }
-        },
-        jshint: {
-            all: {
-                src: watchFiles.clientJS.concat(watchFiles.serverJS),
-                options: {
-                    jshintrc: true
-                }
-            }
-        },
-        csslint: {
+        ngdocs: {
             options: {
-                csslintrc: '.csslintrc',
+                dest: 'docu'
             },
-            all: {
-                src: watchFiles.clientCSS
+            api: {
+                src: ['public/modules/core/**/*.js'],
+                title: 'API Documentation'
             }
         },
-        uglify: {
-            production: {
-                options: {
-                    mangle: false
-                },
-                files: {
-                    'public/dist/application.min.js': 'public/dist/application.js'
-                }
-            }
-        },
-        cssmin: {
-            combine: {
-                files: {
-                    'public/dist/application.min.css': '<%= applicationCSSFiles %>'
-                }
-            }
-        },
-        nodemon: {
-            dev: {
-                script: 'server.js',
-                options: {
-                    nodeArgs: ['--debug'],
-                    ext: 'js,html',
-                    watch: watchFiles.serverViews.concat(watchFiles.serverJS)
-                }
-            }
-        },
-        'node-inspector': {
-            custom: {
-                options: {
-                    'web-port': 1337,
-                    'web-host': 'localhost',
-                    'debug-port': 5858,
-                    'save-live-edit': true,
-                    'no-preload': true,
-                    'stack-trace-limit': 50,
-                    'hidden': []
-                }
-            }
-        },
-        ngAnnotate: {
-            production: {
-                files: {
-                    'public/dist/application.js': '<%= applicationJavaScriptFiles %>'
-                }
-            }
-        },
-        concurrent: {
-            default: ['nodemon', 'watch'],
-            debug: ['nodemon', 'watch', 'node-inspector'],
+        babel: {
             options: {
-                logConcurrentOutput: true,
-                limit: 10
-            }
-        },
-        env: {
-            test: {
-                NODE_ENV: 'test'
+                sourceMap: true,
+                presets: ['env']
             },
-            secure: {
-                NODE_ENV: 'secure'
-            }
-        },
-        jsdoc: {
             dist: {
-                src: ['public/modules/modules.js', 'public/modules/core/**/*.js'],//['app/**/*.js'],
+                files: {
+                    'dist/app.js': 'src/app.js'
+                }
+            }
+        },
+        connect: {
+            server: {
                 options: {
-                    destination: 'doccli'
+                    keepalive: true,
+                    port: 8081,
+                    base: 'docu'
                 }
             }
         }
-
     });
 
-    // Load NPM tasks
-    require('load-grunt-tasks')(grunt);
-
-    //Cargo tarea para generar jsdoc automatico
-    grunt.loadNpmTasks('grunt-jsdoc');
-   
-    // Making grunt default to force in order not to break the project.
-    grunt.option('force', true);
-
-    // A Task for loading the configuration object
-    grunt.task.registerTask('loadConfig', 'Task that loads the config into a grunt option.', function() {
-        var init = require('./config/init')();
-        var config = require('./config/config');
-
-        grunt.config.set('applicationJavaScriptFiles', config.assets.js);
-        grunt.config.set('applicationCSSFiles', config.assets.css);
-    });
-
+    // ngdocs
+    grunt.loadNpmTasks('grunt-ngdocs');
+    grunt.loadNpmTasks('grunt-contrib-connect');
     // Default task(s).
-    grunt.registerTask('default', ['lint', 'concurrent:default']);
-
-    // Debug task.
-    grunt.registerTask('debug', ['lint', 'concurrent:debug']);
-
-    // Secure task(s).
-    grunt.registerTask('secure', ['env:secure', 'lint', 'concurrent:default']);
-
-    // Lint task(s).
-    grunt.registerTask('lint', ['jshint', 'csslint']);
+    grunt.registerTask('default', ['ngdocs']);
+    grunt.registerTask('docu', ['ngdocs', 'connect']);
 
 };
