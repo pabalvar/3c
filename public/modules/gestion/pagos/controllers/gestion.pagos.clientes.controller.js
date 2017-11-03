d5342addb1caf31f59f8b2f491de6b87ef93c43f
pabalvar <pabalvar@b1d87855-21b7-1442-897f-3fc862daeb18>
2017-11-03 12:05:14 +0000

    mejora pagos

--- public/modules/gestion/pagos/controllers/gestion.pagos.clientes.controller.js
+++ public/modules/gestion/pagos/controllers/gestion.pagos.clientes.controller.js
@@ -9,11 +9,6 @@ angular.module('gestion').controller('gestionPagosClientesController', ['$scope'
             { field: "TIEN", name: "Tipo", visible: true, datatype: 'rtabla', tabla: 'TipoEntidad', options: { returnSrv: "id", returnClient: "name" } },
             { field: "SUEN", name: "Suc.", visible: true, pk: true }
         ]
-        $scope.rtablas = {
-            TipoEntidad: { data: [{ id: "A", name: "Ambos" }, { id: "P", name: "Proveedor" }, { id: "C", name: "Cliente" }] },
-            EstadoPago: { data: [{ id: "P", name: "Pendiente" }] }
-
-        }
         $scope.pasoEntidad = [];
         $scope.traeEntidad = (query) => entidades.get({ fields: $scope.metaEntidad.map(m => m.field), search: query.search, size: 10, order: 'NOKOEN' });
 
@@ -22,29 +17,33 @@ angular.module('gestion').controller('gestionPagosClientesController', ['$scope'
             { field: "IDMAEDPCE", name: "IDMAEDPCE", visible: false, pk: true },
             { field: "EMPRESA", name: "Empresa", visible: false },
             { field: "TIDP", name: "TD", description: "Tipo documento", visible: true },
-            { field: "NUDP", name: "Número", visible: true },
-            { field: "ENDP", name: "Emisor", visible: false },
-            { field: "EMDP", name: "Modo", visible: false },
-            { field: "SUEMDP", name: "Sucursal", visible: false },
-            { field: "CUDP", name: "Cudp", visible: false },
-            { field: "NUCUDP", name: "NUCUDP", visible: false },
+            { field: "NUDP", name: "Número", description: "Número documento de pago", visible: true },
+            { field: "ENDP", name: "Entidad", description: "Entidad documento de pago", visible: false },
+            { field: "EMDP", name: "Emisor", description: "Emisor documento de pago (banco)", visible: false },
+            { field: "SUEMDP", name: "Sucursal", description: "Plaza", visible: false },
+            { field: "CUDP", name: "Cuenta", description: "Cuenta de banco", visible: false },
+            { field: "NUCUDP", name: "Número documento", description: "Número del cheque", visible: false },
             { field: "FEEMDP", name: "Emisión", visible: true, datatype: 'date' },
-            { field: "FEVEDP", name: "Vence", visible: true, datatype: 'date' },
+            { field: "FEVEDP", name: "Vencimiento", visible: true, datatype: 'date' },
             { field: "MODP", name: "M", description: "Moneda", visible: true },
-            { field: "TIMODP", name: "Tipo moneda", visible: false },
-            { field: "TAMODP", name: "Tamodp", visible: false },
+            { field: "TIMODP", name: "Tipo moneda", description: "Nacional (N) o extranjera (E)", visible: false },
+            { field: "TAMODP", name: "Tasa de cambio", visible: false },
             { field: "VADP", name: "Monto", visible: true, datatype: 'number' },
-            { field: "VAABDP", name: "Abono", visible: false, datatype: 'number' },
-            { field: "VAASDP", name: "Asignado", visible: true, datatype: 'number' },
-            { field: "VAVUDP", name: "Saldo", visible: true, datatype: 'number' },
-            { field: "ESPGDP", name: "ESPGDP", visible: false },
+            { field: "VAABDP", name: "Abono", description: "Valor abonado al documento de pago", visible: false, datatype: 'number' },
+            { field: "VAASDP", name: "Asignado", description: "Valor asignado a otros documentos", visible: true, datatype: 'number' },
+            { field: "VAVUDP", name: "Vuelto", description: "Vuelto", visible: true, datatype: 'number' },
+            { field: "ESPGDP", name: "Estado", description: "Estado de pago del documento de pago (Pendiente,Cancelado,Nulo)", visible: false },
             { field: "REFANTI", name: "Referencia", visible: true },
             { field: "ARCHIRSD", name: "ARCHIRSD", visible: false },
             { field: "IDRSD", name: "IDRSD", visible: false }
         ]
+        $scope.apiPago = {}
         $scope.pasoPago = [];
-        $scope.traePago = (query) => pagos.get({ fields: $scope.metaPago.map(m => m.field), koen: $scope.pasoEntidad.map(e => e.KOEN),  empresa: rndEmpresa.get(), variante: 'simple', size: 10, order: 'FEEMDP' });
-        $scope.apiPago = {} // Se llena con una funcion dummy
+        $scope.traePago = (query) => pagos.get(
+            { fields: $scope.metaPago.map(m => m.field), koen: $scope.pasoEntidad.map(e => e.KOEN),  empresa: rndEmpresa.get(), variante: 'simple', size: 10, order: 'FEEMDP' },
+            function(res){$scope.pasoPago = res.data; $scope.pasoPago.forEach(function(e){e.$estado = {}})}
+        );
+
 
         /** Trae DEUDA */
         $scope.metaDeuda = [
@@ -65,16 +64,34 @@ angular.module('gestion').controller('gestionPagosClientesController', ['$scope'
             { field: "VANEDO", name: "Valor neto doc.", visible: false, datatype: 'number' },
             { field: "BLOQUEAPAG", name: "Bloquea pago", visible: false }
         ]
+        $scope.apiDeuda = {}
         $scope.pasoDeuda = [];
-        $scope.traeDeuda = (query) => documentos.traeDeuda.get({ fields: $scope.metaDeuda.map(m => m.field), koen: $scope.pasoEntidad.map(e => e.KOEN), empresa: rndEmpresa.get(), size: 10, order: 'FEULVEDO' });
-        $scope.apiDeuda = {} // Se llena con una funcion dummy
+        $scope.traeDeuda = (query) => documentos.traeDeuda.get(
+            { fields: $scope.metaDeuda.map(m => m.field), koen: $scope.pasoEntidad.map(e => e.KOEN), empresa: rndEmpresa.get(), size: 10, order: 'FEULVEDO' },
+            (res)=>$scope.pasoDeuda = res.data
+        );
+
+
+        /* Constantes */
+        $scope.rtablas = {
+            TipoEntidad: { data: [{ id: "A", name: "Ambos" }, { id: "P", name: "Proveedor" }, { id: "C", name: "Cliente" }] },
+            EstadoPago: { data: [{ id: "P", name: "Pendiente" }] }
+        }
+
+        /* Cruce pagos */
+
 
         /** Lógica */
-        //$scope.$watch('pasoEntidad', () => $scope.apiPago.reload ? $scope.apiPago.reload() : '', true);
+        // Si cambia entidad volver a cargar deuda y pagos
         $scope.$watch('pasoEntidad', function(n,o){ 
-            console.log("cambio", n,o);
-            $scope.apiPago.reload ? $scope.apiPago.reload():console.log("no hice na Pagos");
-            $scope.apiDeuda.reload ? $scope.apiDeuda.reload():console.log("no hice na Deuda");
-        }, true)
+            $scope.apiPago.reload ? $scope.apiPago.reload():'';
+            $scope.apiDeuda.reload ? $scope.apiDeuda.reload():'';
+        }, true);
+
+
+
+
+
+
     }
 ])
